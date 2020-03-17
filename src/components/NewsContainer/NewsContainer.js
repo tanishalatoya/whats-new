66eddbb7ed9f0b81e6fbbb966df897fb9b22cb45
Tanisha L Davey <tanisha.davey2@gmail.com>
2020-03-16 19:03:35 -0600

    Use local news as a prop to create news article cards

--- src/components/NewsContainer/NewsContainer.js
+++ src/components/NewsContainer/NewsContainer.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import NewsArticle from '../NewsArticle/NewsArticle';
 import './NewsContainer.css'
 
-const NewsContainer = () => {
+const NewsContainer = ({ localNews }) => {
+  const localNewsCards = localNews.map(localNewsItem => {
+    return <NewsArticle
+              image = {localNewsItem.img}
+              headline = {localNewsItem.headline}
+              description = {localNewsItem.description}
+              url = {localNewsItem.url}
+              id = {localNewsItem.id}
+              key = {localNewsItem.id}
+            />
+  })
+
   return (
     <section className='news-container'>
-      <h2>Local News Here</h2>
+      {localNewsCards}
     </section>
   )
 }
