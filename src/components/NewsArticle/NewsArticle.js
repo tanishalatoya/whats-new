9da0897afe121b8e180bde79ad3c79cee5eac971
Tanisha L Davey <tanisha.davey2@gmail.com>
2020-03-16 19:02:53 -0600

    Create JSX for a news article card

--- src/components/NewsArticle/NewsArticle.js
+++ src/components/NewsArticle/NewsArticle.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import './NewsArticle.css';
 
-// NEWSARTICLE COMPONENT CODE GOES HERE
+const NewsArticle = ({ headline, image, description, url, id }) => {
+  return (
+    <article className='news-article'>
+      <img src={image} alt={headline}/>
+      <h3>{headline}</h3>
+      <p>{description}</p>
+      <div>Link to article<a href={url}> →</a></div>
+    </article>
+  )
+}
 
 export default NewsArticle;
