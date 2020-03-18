e2976c57125de85d3e220c9dc6eb28897d40b76e
Tanisha L Davey <tanisha.davey2@gmail.com>
2020-03-18 09:41:09 -0600

    Add method to update state of input on change

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -4,18 +4,26 @@ import './SearchForm.css';
 class SearchForm extends Component {
   constructor() {
     super();
-    this.state = '';
+    this.state = {
+      search: ''
+    }
+  }
+
+  updateSearchInfo = (event) => {
+    this.setState({ [event.target.name]: event.target.value })
   }
 
   render() {
     return (
       <form>
         <input
+          name='search'
           type='text'
           placeholder='Search for news articles here.'
-          value={this.state}
+          value={this.state.name}
+          onChange={this.updateSearchInfo}
         ></input>
-        <button type='search'>Search Now</button>
+        <button>Search Now</button>
       </form>
     )
   }
