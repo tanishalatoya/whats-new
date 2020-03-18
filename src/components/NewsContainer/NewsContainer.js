48d87d991d6de72427693e066ac26c39f1f729d3
Tanisha L Davey <tanisha.davey2@gmail.com>
2020-03-17 18:42:06 -0600

    Remove unecessary spacing

--- src/components/NewsContainer/NewsContainer.js
+++ src/components/NewsContainer/NewsContainer.js
@@ -5,12 +5,12 @@ import './NewsContainer.css'
 const NewsContainer = ({ localNews }) => {
   const localNewsCards = localNews.map(localNewsItem => {
     return <NewsArticle
-              image = {localNewsItem.img}
-              headline = {localNewsItem.headline}
-              description = {localNewsItem.description}
-              url = {localNewsItem.url}
-              id = {localNewsItem.id}
-              key = {localNewsItem.id}
+              image={localNewsItem.img}
+              headline={localNewsItem.headline}
+              description={localNewsItem.description}
+              url={localNewsItem.url}
+              id={localNewsItem.id}
+              key={localNewsItem.id}
             />
   })
 
