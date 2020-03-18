a5c8f878dc3a2e4b2bb18da42b3d50b41ac54bf4
Tanisha L Davey <tanisha.davey2@gmail.com>
2020-03-18 09:24:12 -0600

    Add class compononent to render search form

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,6 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './SearchForm.css';
 
-// SEARCHFORM COMPONENT CODE GOES HERE
+class SearchForm extends Component {
+  constructor() {
+    super();
+    this.state = '';
+  }
+
+  render() {
+    return (
+      <form>
+        <input
+          type='text'
+          placeholder='Search for news articles here.'
+          value={this.state}
+        ></input>
+        <button type='search'>Search Now</button>
+      </form>
+    )
+  }
+}
 
 export default SearchForm;
