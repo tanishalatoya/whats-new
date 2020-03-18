29f0f21be6e4c8a052fcba7cb096609fcbe8fb08
Tanisha L Davey <tanisha.davey2@gmail.com>
2020-03-17 20:29:14 -0600

    Add method to update the currentNewsTopic

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,21 +2,38 @@ import React, { Component } from 'react';
 import NewsContainer from '../NewsContainer/NewsContainer';
 import Menu from '../Menu/Menu'
 import local from '../../data/local';
+import technology from '../../data/technology';
+import entertainment from '../../data/entertainment';
+import health from '../../data/health';
+import science from '../../data/science';
 import './App.css';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      local
+      local,
+      technology,
+      entertainment,
+      health,
+      science,
+      currentNewsTopic: local
     }
+  }
+
+  updateCurrentNewsTopic = (topic) => {
+    const stateKeys = Object.keys(this.state)
+    const currentKey = stateKeys.find(key => key === topic)
+    this.setState({ currentNewsTopic: this.state[currentKey] })
   }
 
   render () {
     return (
       <main className="app">
-        <Menu />
-        <NewsContainer localNews={this.state.local}/>
+        <Menu updateCurrentNewsTopic={this.updateCurrentNewsTopic} />
+        <NewsContainer
+          currentNewsTopic={this.state.currentNewsTopic}
+          />
       </main>
     );
   }
