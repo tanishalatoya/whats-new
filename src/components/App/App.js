e16083630b313ce118e7ba6eb7861a1033b5f152
Tanisha L Davey <tanisha.davey2@gmail.com>
2020-03-17 18:40:58 -0600

    Add Menu to render method

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import NewsContainer from '../NewsContainer/NewsContainer';
+import Menu from '../Menu/Menu'
 import local from '../../data/local';
 import './App.css';
 
@@ -14,6 +15,7 @@ class App extends Component {
   render () {
     return (
       <main className="app">
+        <Menu />
         <NewsContainer localNews={this.state.local}/>
       </main>
     );
