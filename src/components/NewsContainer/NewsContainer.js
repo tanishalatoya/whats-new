0cd0d9239c2de99be634843afb5b1eff5a42783c
Tanisha L Davey <tanisha.davey2@gmail.com>
2020-03-17 20:31:09 -0600

    Change prop to currentNewsTopic

--- src/components/NewsContainer/NewsContainer.js
+++ src/components/NewsContainer/NewsContainer.js
@@ -2,21 +2,21 @@ import React from 'react';
 import NewsArticle from '../NewsArticle/NewsArticle';
 import './NewsContainer.css'
 
-const NewsContainer = ({ localNews }) => {
-  const localNewsCards = localNews.map(localNewsItem => {
+const NewsContainer = ({ currentNewsTopic }) => {
+  const newsCards = currentNewsTopic.map(newsItem => {
     return <NewsArticle
-              image={localNewsItem.img}
-              headline={localNewsItem.headline}
-              description={localNewsItem.description}
-              url={localNewsItem.url}
-              id={localNewsItem.id}
-              key={localNewsItem.id}
+              image={newsItem.img}
+              headline={newsItem.headline}
+              description={newsItem.description}
+              url={newsItem.url}
+              id={newsItem.id}
+              key={newsItem.id}
             />
   })
 
   return (
     <section className='news-container'>
-      {localNewsCards}
+      {newsCards}
     </section>
   )
 }
