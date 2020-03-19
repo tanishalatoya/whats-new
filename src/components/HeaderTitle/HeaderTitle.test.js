aa060842f4feba7eafb555d0ecc1e1f59895c00f
Tanisha L Davey <tanisha.davey2@gmail.com>
2020-03-19 14:37:56 -0600

    Set up describe and it block for testing

---
+++ src/components/HeaderTitle/HeaderTitle.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import HeaderTitle from './HeaderTitle';
+import { render } from '@testing-library/react';
+
+describe('HeaderTitle', () => {
+  it('should display an h1 with text', () => {
+    
+  });
+})
