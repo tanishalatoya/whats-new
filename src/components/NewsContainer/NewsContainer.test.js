400f11aecebc666b1378c716bb5058aff6ec129c
Tanisha L Davey <tanisha.davey2@gmail.com>
2020-03-19 16:34:51 -0600

    Begin testing NewsContainer component

--- src/components/NewsContainer/NewsContainer.test.js
+++ src/components/NewsContainer/NewsContainer.test.js
@@ -4,10 +4,18 @@ import { render } from '@testing-library/react';
 
 describe('NewsContainer', () => {
   it('should display a news article', () => {
-    const { getByText } = render(<NewsContainer
-      
-
-      />)
+    // const { getByText } = render(<NewsContainer>
+    //
+    //   how to render the article inside the container and then check that it's there???
+    //
+    //   <article id={id} className='news-article'>
+    //     <img src={image} alt={headline}/>
+    //     <h3>{headline}</h3>
+    //     <p className='description'>{description}</p>
+    //     <div><a href={url}>Link to article →</a></div>
+    //   </article>
+    //
+    //   <NewsContainer/>)
 
   });
 })
